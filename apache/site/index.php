cf4aa462aa291272d47e707dd72b3d7c8ecb13d9
VVV-VVV <M.M4p@ya.ru>
2024-09-19 15:17:08 +0300

    no_site

---
+++ apache/site/index.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h1>Welcome to the Dynamic Page 1</h1>";
+echo "<p>This is served by Apache and PHP.</p>";
+?>
---
+++ apache/site/page2.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h1>Welcome to the Dynamic Page 2</h1>";
+echo "<p>This is another dynamic page served by Apache.</p>";
+?>
